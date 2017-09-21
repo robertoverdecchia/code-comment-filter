e44aff0e0cc82b37a00e0ff98c9159d2572f7059
robertoverdecchia <roberto.verdecchia@tiscali.it>
2017-09-21 18:43:44 +0200

    Refactored parse.py

--- parse.py
+++ parse.py
@@ -5,29 +5,29 @@ import shutil
 from comment_parser import comment_parser
 import git
 from git import Repo
-        
-git_repository_url = "https://github.com/r0ket/r0ket.git" # URL of git repository
-MIME = "text/x-c" # MIME type
 
-L = list()                  
+git_repository_url = "https://github.com/r0ket/r0ket.git"  # URL of git repository
+MIME = "text/x-c"  # MIME type
+
+L = list()
 C = list()
 i = 0
 
 print "Cloning repository..."
-Repo.clone_from(git_repository_url, "repository_root")   
+Repo.clone_from(git_repository_url, "repository_root")
 print "Repository cloning ended"
 
 print "Comment parsing started..."
-for root, dirs, files in os.walk("repository_root"): # recursively find files starting from root folder
+# recursively find files starting from root folder
+for root, dirs, files in os.walk("repository_root"):
     for filename in files:
-        if filename.lower().endswith((".c", ".h")): # filter for given file extention
-            L.append(os.path.join(root, filename))              
-            c = (comment_parser.extract_comments(L[i], MIME))  # parse comments according to 
-                                                                     # hardcoded MIME and store them 
-                                                                     # in list
+        if filename.lower().endswith((".c", ".h")):  # filter for given file extention
+            L.append(os.path.join(root, filename))
+            # parse comments according to hardcoded MIME
+            c = (comment_parser.extract_comments(L[i], MIME))
+            # store comment in list
             C.append(c)
             i += 1
-            #print C[i]
 print "Comment parsing ended"
 
 
@@ -36,15 +36,17 @@ with open("patterns.txt") as f:           # patterns to be matched
 
 print "Pattern matching started..."
 with open('output_parsing.tsv', 'w') as csvfile:
-    writer=csv.writer(csvfile, delimiter='\t')
-    writer.writerow(["File name", "Keyword",  "Comment"]) # write header
+    writer = csv.writer(csvfile, delimiter='\t')
+    writer.writerow(["File name", "Keyword",  "Comment"])  # write header
 
     for x in range(len(L)):               # for each file
         for y in range(len(C[x])):        # for each comment
-            for z in range (len(keywords)):
-                s = str(C[x][y])  
-                if keywords[z].lower() in s.lower(): # match lower case for case insensivity
-                    writer.writerow([L[x], keywords[z],  str(C[x][y])]) # store file name, keyword and comment
+            for z in range(len(keywords)):
+                s = str(C[x][y])
+                # match lower case for case insensivity
+                if keywords[z].lower() in s.lower():
+                    # store file name, keyword and comment
+                    writer.writerow([L[x], keywords[z],  str(C[x][y])])
 print "Pattern matching ended"
 
-shutil.rmtree("repository_root") # remove local copy of cloned repository
+shutil.rmtree("repository_root")  # remove local copy of cloned repository
